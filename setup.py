3bc34025ce9782d300052bd442a1b88067e52f0e
Joshua Carp <jm.carp@gmail.com>
2015-04-26 10:20:56 -0400

    Remove requirements parsing.
    [fixes #11]
    [fixes #9]
    [fixes #5]

--- setup.py
+++ setup.py
@@ -5,9 +5,17 @@ import re
 import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
-from pip.req import parse_requirements
 
 
+INSTALL_REQUIRES = [
+    'six>=1.9.0',
+    'enum34>=1.0.4',
+    'invoke>=0.10.1',
+    'requests>=2.6.2',
+    'decorator>=3.4.2',
+    'inflection>=0.3.0',
+    'python-dateutil>=2.4.2',
+]
 TEST_REQUIRES = [
     'pytest',
     'responses',
@@ -23,9 +31,7 @@ class PyTest(TestCommand):
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = [
-            '--verbose'
-        ]
+        self.test_args = ['--verbose']
         self.test_suite = True
 
     def run_tests(self):
@@ -37,7 +43,6 @@ class PyTest(TestCommand):
 def find_version(fname):
     """Attempts to find the version number in the file names fname.
     Raises RuntimeError if not found.
-
     """
     version = ''
     with open(fname, 'r') as fp:
@@ -51,17 +56,16 @@ def find_version(fname):
         raise RuntimeError('Cannot find version information')
     return version
 
-__version__ = find_version('betfair/__init__.py')
-
 
 def read(fname):
     with open(fname) as fp:
         content = fp.read()
     return content
 
+
 setup(
     name='betfair.py',
-    version=__version__,
+    version=find_version('betfair/__init__.py'),
     description='Python client for the Betfair API '
                 '(https://api.developer.betfair.com/)',
     long_description=open('README.rst').read(),
@@ -71,23 +75,20 @@ setup(
     packages=find_packages(exclude=('test*', )),
     package_dir={'betfair': 'betfair'},
     include_package_data=True,
-    install_requires=[
-        str(requirement.req)
-        for requirement in parse_requirements('requirements.txt')
-    ],
+    install_requires=INSTALL_REQUIRES,
     license=read('LICENSE'),
     zip_safe=False,
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     test_suite='tests',
     tests_require=TEST_REQUIRES,
