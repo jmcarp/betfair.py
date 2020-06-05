4890bfc5448791951b34b7cbeb56d81e5f4d8e9d
senegrom <6349874+senegrom@users.noreply.github.com>
2020-06-05 16:27:29 +0100

    Update betfair.py

--- betfair/betfair.py
+++ betfair/betfair.py
@@ -17,7 +17,7 @@ from betfair import exceptions
 
 
 IDENTITY_URLS = collections.defaultdict(
-    lambda: 'https://identitysso.betfair.com/api/',
+    lambda: 'https://identitysso-cert.betfair.com/api/',
     italy='https://identitysso.betfair.it/api/',
 )
 
